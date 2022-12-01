dfec4fd58c3ef8801d5f6457edd4f298d7d5ed9c
qntmcube <76634041+Java4Jedi@users.noreply.github.com>
2022-11-30 19:29:38 -0800

    Copied code

--- src/main/java/com/team766/robot/OI.java
+++ src/main/java/com/team766/robot/OI.java
@@ -15,7 +15,8 @@ public class OI extends Procedure {
 	private JoystickReader joystick0;
 	private JoystickReader joystick1;
 	private JoystickReader joystick2;
-	
+	private double elevatorUp;
+
 	public OI() {
 		loggerCategory = Category.OPERATOR_INTERFACE;
 
@@ -25,11 +26,46 @@ public class OI extends Procedure {
 	}
 	
 	public void run(Context context) {
-		while (true) {
+		context.takeOwnership(Robot.drive);
+		context.takeOwnership(Robot.elevator);
+		//PS4Controller ourPS4 = new PS4Controller(0); // 0 is the USB Port to be used as indicated on the Driver Station, and ourPS4 is the object name of our PS4 controller.
+		
+		while (2>1) {
+
+			//REMEMBER: LEFTJOYSTICK1 IS THE X AXIS OF THE LEFT JOYSTICK. LEFTJOYSTICK2 IS THE Y AXIS OF THE LEFT JOYSTICK>
+			//CURRENTLY SET TO SPLIT ARCADE DRIVE
+
+			double leftJoystick1 = joystick0.getAxis(1);
+			double leftJoystick2 = joystick0.getAxis(0);
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
-			
+			/**log("J0 A0: " + joystick0.getAxis(0) +
+			    "  J0 A1: " + joystick0.getAxis(1) +
+			    "  J1 A0: " + joystick1.getAxis(0) +
+			    "  J1 A1: " + joystick1.getAxis(1) +
+			    "  J0 B1: " + joystick0.getButton(1) +
+			    "  J0 B2: " + joystick0.getButton(2) +
+			    "  J0 B3: " + joystick0.getButton(3));
+**/
+			log("J0 A1: " + leftJoystick1);
+			log(" J0 A0:" + leftJoystick2);
 
+			//Robot.drive.setArcadeDrivePower(leftJoystick1, leftJoystick2);
+			Robot.drive.setArcadeDrivePower(leftJoystick1, leftJoystick2);     
+			
+			if(joystick0.getButton(2)){
+				elevatorUp = 1.0;
+				log("up button pressed");
+			}
+			else if(joystick0.getButton(3)){
+				elevatorUp = -1.0;
+			}
+			else {
+				elevatorUp = 0.0;
+			}
+
+			Robot.elevator.setMotorPower(elevatorUp);
+			log(" Elevator: " + elevatorUp);
 			context.waitFor(() -> RobotProvider.instance.hasNewDriverStationData());
 		}
 	}
--- src/main/java/com/team766/robot/Robot.java
+++ src/main/java/com/team766/robot/Robot.java
@@ -4,10 +4,14 @@ import com.team766.robot.mechanisms.*;
 
 public class Robot {
 	// Declare mechanisms here
-	
+	public static Drive drive;
+	public static Elevator elevator;
+	public static OI m_oi;
 
 	public static void robotInit() {
 		// Initialize mechanisms here
-		
+		drive = new Drive();
+		elevator = new Elevator();
+		m_oi = new OI();
 	}
 }
